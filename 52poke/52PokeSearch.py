bc5bf67d2705daeda0017ddff586beb06a04ed9c
Passerby <450343225@qq.com>
2018-06-28 17:43:06 +0800

    增加获取宝可梦信息的脚本

--- 52poke/52PokeSearch.py
+++ 52poke/52PokeSearch.py
@@ -4,3 +4,13 @@
 from urllib import request
 from bs4 import BeautifulSoup
 import re, os, sys, time
+
+pokemonListBase = "https://wiki.52poke.com/wiki/%E5%AE%9D%E5%8F%AF%E6%A2%A6%E5%88%97%E8%A1%A8%EF%BC%88%E6%8C%89%E5%85%A8%E5%9B%BD%E5%9B%BE%E9%89%B4%E7%BC%96%E5%8F%B7%EF%BC%89"
+
+def getPokemonInfo(pokemonListPage):
+    soup = BeautifulSoup(pokemonListPage.text, 'lxml')
+
+
+if __name__ == '__main__':
+    pokemonListPage = requests.get(pokemonListBase)
+    getPokemonInfo(pokemonListPage)
