9c85f412f8a47c78f2e3ec7ef77e4b1949a82a24
Passerby <450343225@qq.com>
2018-07-03 17:55:27 +0800

    修改sqlite操作脚本，使用?占位符 已完成： 1. 创建数据库（简单创建，不能有索引一类的东西） 2. 删除数据库 3. 数据表的增删改查

--- 52poke/dbHandler.py
+++ 52poke/dbHandler.py
@@ -24,17 +24,18 @@ class ObjSqliteConnector(object):
         try:
             if not self.conFlag:
                 self._Connect()
+            print sql
             self.dbCursor.execute(sql)
             self.dbConn.commit()
-            return True
+            return (True, 'successful')
         except:
-            print traceback.format_exc()
-            return False
+            return (False, traceback.format_exc())
 
     def _executeSqlFormat(self, sql, params):
         try:
             if not self.conFlag:
                 self._Connect()
+            print sql, params
             self.dbCursor.execute(sql, params)
             self.dbConn.commit()
             return (True, 'successful')
@@ -45,6 +46,7 @@ class ObjSqliteConnector(object):
         try:
             if not self.conFlag:
                 self._Connect()
+            print sql, params
             self.dbCursor.executemany(sql, params)
             self.dbConn.commit()
             return (True, 'successful')
@@ -55,18 +57,27 @@ class ObjSqliteConnector(object):
         try:
             if not self.conFlag:
                 self._Connect()
+            print sql
             self.dbCursor.execute(sql)
             retList = self.dbCursor.fetchall()
             if len(retList) <= 0 and type(retList) != list:
                 retList = None
-            return retList
+            return (True, retList)
         except:
-            print traceback.format_exc()
-            return None
-
+            return (False, traceback.format_exc())
 
     def _querySqlFormat(self, sql, params):
-        pass
+        try:
+            if not self.conFlag:
+                self._Connect()
+            print sql, params
+            self.dbCursor.execute(sql, params)
+            retList = self.dbCursor.fetchall()
+            if len(retList) <= 0 and type(retList) != list:
+                retList = None
+            return (True, retList)
+        except:
+            return (False, traceback.format_exc())
 
     def createTable(self, tableName, tableColumns):
         self.dropTable(tableName)
@@ -79,57 +90,114 @@ class ObjSqliteConnector(object):
 
     def insert(self, tableName, values, columns = ''):
         try:
-            bRet, Reason = False, 'System Error'
+            bRet, Result = False, 'System Error'
             if type(values) != list and type(values[0]) != tuple:
-                return (False, 'values is not fit for [(),]')
+                bRet, Result = False, 'values is not fit for [(),]'
+                return
             valuesLen = len(values)
             colunmLen = len(values[0])
             if colunmLen <= 0 or valuesLen <= 0:
-                return (False, 'there has not data to insert')
+                bRet, Result = False, 'there has not data to insert'
+                return
             insertSqlFormat = 'INSERT INTO `{0}` {1} values (?' + ',?' * (colunmLen - 1) + ');'
             insertSql = insertSqlFormat.format(tableName, columns)
-            print insertSql, valuesLen
             if valuesLen == 1:
-                bRet, Reason = self._executeSqlFormat(insertSql, values[0])
+                bRet, Result = self._executeSqlFormat(insertSql, values[0])
             else:
-                bRet, Reason = self._executeMany(insertSql, values)
+                bRet, Result = self._executeMany(insertSql, values)
         except:
-            bRet, Reason = False, traceback.format_exc()
+            bRet, Result = False, traceback.format_exc()
         finally:
-            return (bRet, Reason)
+            return (bRet, Result)
 
     def select(self, tableName, columns = '*', whereClause = '', whereValue = ''):
         try:
-            bRet, Reason = False, 'System Error'
+            bRet, Result = False, 'System Error'
             if columns == '' : columns = '*'
             valueCount = 0
             if whereClause != '':
                 valueCount = len(whereValue)
-                clauseCount = whereClaus.count('?');
-                if valueCount != clauseCount and type(whereValue) != tuple:
-                    return (False, 'whereData not fit whereClaus')
+                clauseCount = whereClause.count('?');
+                if valueCount != clauseCount or type(whereValue) != tuple:
+                    bRet, Result = False, 'whereData not fit whereClause'
+                    return
             selectSqlFormat = "select {0} from {1}"
             if valueCount != 0:
-                selectSql = selectSqlFormat.format(columns, tableName) + ' where ' + whereClaus + ';'
-                bRet, Reason = self._querySqlFormat(selectSql, whereValue)
+                selectSql = selectSqlFormat.format(columns, tableName) + ' where ' + whereClause + ';'
+                bRet, Result = self._querySqlFormat(selectSql, whereValue)
             else:
                 selectSql = selectSqlFormat.format(columns, tableName) + ';'
-                bRet, Reason = self._querySql(selectSql);
+                bRet, Result = self._querySql(selectSql);
         except:
-            bRet, Reason = False, traceback.format_exc()
+            bRet, Result = False, traceback.format_exc()
         finally:
-            return (bRet, Reason)
+            return (bRet, Result)
 
-    def queryGetId(self, tableName, columns = None, whereClause = None):
-        retInt = self.query(tableName, columns, whereClause)
-        if retInt != None:
-            if str(retInt[0][0]).isdigit():
-                retInt = int(retInt[0][0])
+    def selectAndGetId(self, tableName, columns = '*', whereClause = '', whereValue = ''):
+        bRet, Result = self.select(tableName, columns, whereClause, whereValue)
+        if bRet:
+            if str(Result[0][0]).isdigit():
+                Result = int(Result[0][0])
             else:
-                retInt = None
-        return retInt
+                bRet, Result = False, 'select result is not a integer'
+        return bRet, Result
 
+    def update(self, tableName, columns, values, whereClause = '', whereValue = ''):
+        try:
+            bRet, Result = False, 'System Error'
+            if type(columns) != tuple or type(values) != tuple:
+                bRet, Result = False, 'columns or values type is not tuple'
+                return
+            columnCount = len(columns)
+            valuesCount = len(values)
+            if columnCount != valuesCount:
+                bRet, Result = False, 'columns not fit values'
+                return
+            whereValueCount = 0
+            if whereClause != '':
+                whereValueCount = len(whereValue)
+                whereClauseCount = whereClause.count('?');
+                if whereValueCount != whereClauseCount or type(whereValue) != tuple:
+                    bRet, Result = False, 'whereData not fit whereClause'
+                    return
+            columnStr = ''
+            for item in columns:
+                target = '`' + str(item) + '`=?,'
+                columnStr += target
+            columnStr = columnStr[:-1]
+            updateSqlFormat = "UPDATE {0} SET {1}"
+            if whereValueCount != 0:
+                updateSql = updateSqlFormat.format(tableName, columnStr) + ' where ' + whereClause + ';'
+                bRet, Result = self._executeSqlFormat(updateSql, values + whereValue)
+            else:
+                updateSql = updateSqlFormat.format(tableName, columnStr) + ';'
+                bRet, Result = self._executeSqlFormat(updateSql, values)
+        except:
+            bRet, Result = False, traceback.format_exc()
+        finally:
+            return (bRet, Result)
 
+    def delete(self, tableName, whereClause = '', whereValue = ''):
+        try:
+            bRet, Result = False, 'System Error'
+            valueCount = 0
+            if whereClause != '':
+                valueCount = len(whereValue)
+                clauseCount = whereClause.count('?');
+                if valueCount != clauseCount or type(whereValue) != tuple:
+                    bRet, Result = False, 'whereData not fit whereClause'
+                    return
+            deleteSql = "DELETE FROM " + tableName
+            if valueCount != 0:
+                deleteSql = deleteSql + ' where ' + whereClause + ';'
+                bRet, Result = self._executeSqlFormat(deleteSql, whereValue)
+            else:
+                deleteSql = deleteSql + ';'
+                bRet, Result = self._executeSql(deleteSql);
+        except:
+            bRet, Result = False, traceback.format_exc()
+        finally:
+            return (bRet, Result)
 
 if __name__ == '__main__':
     sqliteConn = ObjSqliteConnector("./test.sqlite")
@@ -140,7 +208,12 @@ if __name__ == '__main__':
     print sqliteConn.insert('abc', [(6, 'f'), (7, 'g')], '(id, info)')
     print sqliteConn.insert('abc', [(8, 'h')], '(id, name)')
     print sqliteConn.insert('abc', [(9, 'i', 'i')])
-    print sqliteConn.query('abc')
-    print sqliteConn.query('abc', ['id'], 'id=5')
-    print sqliteConn.queryGetId('abc', ['info'], 'id=5')
+    print sqliteConn.select('abc')
+    print sqliteConn.select('abc', '(id)', 'id=?', (5,))
+    print sqliteConn.selectAndGetId('abc', ('info'), 'id=?', (5,))
+    print sqliteConn.update('abc', 'dfd', 'dfd')
+    print sqliteConn.update('abc', ('name', 'info'), ('test', 'test'))
+    print sqliteConn.update('abc', ('id', 'name'), (100, 'hahaha'), 'id=?', (1, ))
+    print sqliteConn.delete('abc')
+    print sqliteConn.delete('abc', 'id=?', (5,))
     #sqliteConn.dropTable('abc')
