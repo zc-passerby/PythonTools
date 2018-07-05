95ffab60d3ab2314ae1ab29e1ed69d57adb9a24a
Passerby <450343225@qq.com>
2018-07-05 18:29:35 +0800

    增加宝可梦名词定义，都是一些字典，中英文互转的

---
+++ 52poke/pokeNounsDefine.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+#-*- coding=utf-8 -*-
+
+RegionDefine = {
+    u'全国': 'National',
+    u'关都': 'Kanto',
+    u'城都': 'Johto',
+    u'丰缘': 'Hoenn',
+    u'神奥': 'Sinnoh',
+    u'合众': 'Unova',
+    u'卡洛斯': 'Kalos',
+    u'阿罗拉': 'Alola'
+}
